1515fa393624d3d86b3a8278ecf0ed8db9127e21
Nilo Neregato <neregato.nilo@gmail.com>
2022-08-23 05:26:53 +0200

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
+import styles from "./home.module.scss";
+
 import Title from "../components/Title";
 
 export default function Home() {
   return (
     <>
       <Title title="home | ig.news" />
-      <h1>hello</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome!</span>
+
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
